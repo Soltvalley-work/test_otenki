57c6f5b98db957588d6d5ec4426ec016bf2fa574
Soltvalley-work <75231207+Soltvalley-work@users.noreply.github.com>
2020-12-18 09:28:35 +0900

    新規作成
    HP航海用

---
+++ sw.js
@@ -0,0 +1,33 @@
+
+// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+
+// キャッシュ名とキャッシュファイルの指定
+var CACHE_NAME = 'test-shio-caches';
+var urlsToCache = [
+	'/test/',
+	'/test/src/index.css',
+	'/test/src/index.js',
+	'/test/public/logo192.png'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
